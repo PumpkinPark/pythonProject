17f48d6c13beb2cfa811ccaf5f2f28f37f96566f
tasde <tasdelly@gmail.com>
2022-02-14 23:59:50 +0900

    22.2.14 class

--- last.py
+++ last.py
@@ -1,8 +1,5 @@
-# class Gesan:
-#     pass
-#
-# a = Gesan()
-# print(type(a))
+
+
 
 class Gesan:
     def setdata(self, first, second):
@@ -13,3 +10,15 @@ a = Gesan()
 a.setdata(1,2) # a = self , 1 = first, 2 = second
 print(a.first)
 print(a.second)
+
+# class Alapa:
+#     def dataset(self, itzy, ni):
+#         self.itzy = itzy
+#         self.ni = ni
+#
+# b = Alapa()
+# b.dataset(5,10)
+# print(b.itzy)
+# print(b.ni)
+
+
